bff71a60b73a811b2904d3a35e4d9e821e1b82f2
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-07-12 18:32:04 +0300

    Playground 3D cube.

---
+++ playground/p01_3d_cube/index.js
@@ -0,0 +1,11 @@
+(function(window, document, $, TweenMax) {
+    $(function() {
+        
+        // TweenMax.to("#my-cube", 5, {
+        //     rotationX: 30,
+        //     rotationY: 30,
+        //     rotationZ: 30,
+        // });
+
+    });
+})(window, document, $, TweenMax);
