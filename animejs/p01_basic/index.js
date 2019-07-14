f8a105c2463c6f24a33886f3fe342b9878a4f96f
Andrei-George Hondrari <andrei.hondrari@gmail.com>
2019-07-15 01:13:17 +0300

    Basic animejs.

---
+++ animejs/p01_basic/index.js
@@ -0,0 +1,11 @@
+$(function() {
+    anime({
+        targets: ".box",
+        translateX: 200,
+        autoplay: true,
+        loop: true,
+        direction: "alternate",
+        duration: 1000,
+        easing: "linear"
+    })
+});
